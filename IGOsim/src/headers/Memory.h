6c11068a564800d9805092b410f579c7c2b21d73
Vladislav Fitc <vladislavfitz@me.com>
2014-02-19 22:56:24 +0100

    Class Memory
    - cellules ajoutées
    - les méthodes implementés

--- IGOsim/src/headers/Memory.h
+++ IGOsim/src/headers/Memory.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <unordered_map>
+#include <string>
+#include <sstream>
+#include <iostream>
 
 /*!
 * \class Memory
@@ -6,17 +10,35 @@
 *
 * La mémoire est ici simplement modélisée comme une liste de clées et de valeurs, associée à des contraintes.
 */
-
+template <class T>
 class Memory
 {
 private:
-
+    std::unordered_map<std::string, T> cells; /*!< Les cellules de memoire */
 public:
     /*!
     * \fn Memory()
     * \brief Constructeur
     */
     Memory();
+    
+    /*!
+     * \fn Memory(std::unordered_map<std::string, T>)
+     * \brief Constructeur parametrisŽ avec cells
+     */
+    Memory(std::unordered_map<std::string, T>);
+    
+    /*!
+     * \fn  unsigned int getSize();
+     * \brief Retourne la taille courante de la memoire
+     */
+    unsigned int getSize();
+    
+    /*!
+     * \fn  int setValueForKey(std::string key, T value);;
+     * \brief ƒcrit une valeur "value" dans la case memoire avec un nom "key"
+     */
+    int setValueForKey(std::string, T);
 
     /*!
     * \fn ~Memory()
--- IGOsim/src/includes/Memory.cpp
+++ IGOsim/src/includes/Memory.cpp
@@ -1,11 +1,30 @@
-#include <Memory.h>
+#include "Memory.h"
 
 
-Memory::Memory()
+template<class T>
+Memory<T>::Memory()
 {
 }
 
-
-Memory::~Memory()
+template<class T>
+Memory<T>::Memory(std::unordered_map<std::string, T> cells)
 {
+    this->cells = cells;
 }
+
+template<class T>
+unsigned int Memory<T>::getSize(){
+    return this->cells.size;
+}
+
+template <class T>
+int Memory<T>::setValueForKey(std::string key, T value){
+    this->cells[key] = value;
+}
+
+template<class T>
+Memory<T>::~Memory()
+{
+    cells.clear();
+}
+
