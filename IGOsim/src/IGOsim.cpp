b7a0b6f65f5c8ffd67c6080bf57cde92365da7af
Vladislav Fitc <vladislavfitz@me.com>
2014-02-20 16:44:22 +0100

    igosim

--- IGOsim/src/IGOsim.cpp
+++ IGOsim/src/IGOsim.cpp
@@ -4,11 +4,21 @@
 #include <iostream>
 
 #include "Module.h"
+#include "MacroModule.h"
+#include "Socket.h"
 
 
 int main()
 {
     std::cout << "Hello world" << std::endl;;
+    Memory<int> mem;
+    Socket pin1("pin1", Socket::IN);
+    Socket pin2("pin2", Socket::OUT);
+    MacroModule macro(mem);
+    macro.addSocket(pin1);
+    macro.addSocket(pin2);
+    
+
 	return 0;
 }
 
