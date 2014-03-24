d83276d726f79742c7baf29e0f7c5d84b41d96a3
Ulysse <thevoodoos@gmail.com>
2014-03-24 22:44:19 +0100

    Le même sans l'access violation...

---
+++ IGOsim/src/Core/XMLReader.cpp
@@ -0,0 +1,51 @@
+﻿#include "XMLReader.h"
+
+#include <iostream>
+
+#include "CLI.h"
+
+#include "RapidXML\rapidxml.hpp"
+#include "RapidXML\rapidxml_utils.hpp"
+
+using namespace std;
+
+//Initialisation variable statique:
+string XMLReader::basePath = "config/";
+
+XMLReader::~XMLReader() {
+
+}
+
+
+unordered_map<string, double> XMLReader::readParams(string confPath) {
+
+    //On tente d'ouvrir le fichier:
+    cout << "Reading from: " << basePath + confPath;
+
+    rapidxml::xml_document<> doc;
+
+    try {
+        rapidxml::file<char> xmlFile((basePath+confPath).c_str());
+        doc.parse<0>(xmlFile.data());
+        cout << "Name of my first node is: " << doc.first_node()->name() << "\n";
+        return  unordered_map<string, double>();
+    }
+    catch (const runtime_error &error){
+        //Exception non bloquante:
+        CLI::getInstance().log(CLI::WARNING, "Impossible de lire le fichier de configuration "+basePath+confPath+"\n"+error.what());
+        throw;
+    }
+    catch (rapidxml::parse_error &e) {
+        CLI::getInstance().log(CLI::ERROR, "Erreur de syntaxe dans le fichier de configuration " + basePath + confPath);
+        //Exception bloquante !
+    }
+}
+
+
+void XMLReader::setPath(string p) {
+    basePath = p;
+}
+
+string XMLReader::getPath() {
+    return basePath;
+}
---
+++ IGOsim/src/Core/XMLReader.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include <string>
+#include <unordered_map>
+
+/*!
+* \class XMLReader
+* \brief Classe utilitaire pour la lecture des fichiers d econfiguration
+*
+* Cette classe singleton fournit des méthodes statiques permettant par exemple de lire la configuration initiale d'un module
+*/
+
+class Message;
+class XMLReader
+{
+private:
+    static std::string basePath; /*! Le chemin vers le dossier de configuration */
+public:
+    /*!
+    * \fn static XMLReader& getInstance()
+    * \brief Retourne l'instance unique du reader
+    */
+    static XMLReader& getInstance()
+    {
+        static XMLReader instance;
+        return instance;
+    }
+
+
+    /*!
+    * \fn static std::unordered_map<std::string, double> readParams(std::string moduleName)
+    * \brief Lecture des paramètre du module protant ce nom.
+    * Cette fonction recherche le fichier de configuration portant le nom du module, appelle le parser RapidXML puis créé le tableau de paramètres.
+    * \todo En cas d'échec, elle lève une exception.
+    */
+    static std::unordered_map<std::string, double> readParams(std::string);
+
+    /*!
+    * \fn static std::string getPath() const
+    * \brief Getter statique du chemin vers le dossier de configuration
+    */
+    static std::string getPath();
+
+
+    /*!
+    * \fn static void setPath(std::string)
+    * \brief Setter statique du chemin vers le dossier de configuration
+    */
+    static void setPath(std::string);
+
+    /*!
+    * \fn ~XMLReader()
+    * \brief Destructeur
+    */
+    ~XMLReader();
+
+private:
+    XMLReader() {};
+    XMLReader(XMLReader const&);          /*!< À ne pas implementer */
+    void operator=(XMLReader const&);     /*!< À ne pas implementer */
+};
